ea71ef8d71b1b2f17cfdb003c7e4d4d63481ebf1
Wookie <lukasputon@gmail.com>
2019-12-15 12:39:42 +0100

    V 0.1 Improved visualisation of bus lines thanks to using subplots

--- Visualize.py
+++ Visualize.py
@@ -37,14 +37,17 @@ def visualize_best_route(route: nx.Graph, best_sol: List):
     )
     nx.draw_networkx_labels(route, pos, font_size=20)
     plt.show()'''
+
     pos = nx.spring_layout(route)
-    nodes_basic = nx.draw_networkx_nodes(route, pos, node_size=600)
-    edges_basic = nx.draw_networkx_edges(
-        route,
-        pos,
-        width=1
-    )
-    nx.draw_networkx_labels(route, pos, font_size=10)
+
+    #nodes_basic = nx.draw_networkx_nodes(route, pos, node_size=600)
+    #edges_basic = nx.draw_networkx_edges(
+     #   route,
+      #  pos,
+       # width=1
+    #)
+    #nx.draw_networkx_labels(route, pos, font_size=10)
+
     bus_lines_edges = []
     for bus in best_sol[0]:
         bus_line_edges = []
@@ -59,15 +62,44 @@ def visualize_best_route(route: nx.Graph, best_sol: List):
         directed_graph_lines.append(nx.DiGraph())
         directed_graph_lines[-1].add_edges_from(line)
 
+    if len(directed_graph_lines)>1:
+        fig, ax = plt.subplots(len(directed_graph_lines))
 
+        for idx, line_graph in enumerate(directed_graph_lines):
+            nx.draw_networkx_nodes(route, pos, node_size=600, ax=ax[idx])
+            nx.draw_networkx_edges(
+                route,
+                pos,
+                ax=ax[idx],
+                width=1
+              )
+            nx.draw_networkx_labels(route, pos, font_size=10, ax=ax[idx])
+            nx.draw_networkx_edges(
+                line_graph,
+                pos,
+                edge_color=(np.random.rand(), np.random.rand(), np.random.rand()),
+                arrowsize=15,
+                width=2,
+                ax=ax[idx]
 
-    for idx, line_graph in enumerate(directed_graph_lines):
+            )
+            ax[idx].set_title('Linia numer {}'.format(idx+1), fontsize=10)
+    else:
+        nx.draw_networkx_nodes(route, pos, node_size=600,)
         nx.draw_networkx_edges(
-            line_graph,
+            route,
             pos,
-            edge_color=(0.1*(idx+1), 0.4, 0.1*(idx+1)),
+            width=1
+        )
+        nx.draw_networkx_labels(route, pos, font_size=10)
+        nx.draw_networkx_edges(
+            directed_graph_lines[0],
+            pos,
+            edge_color=(np.random.rand(), np.random.rand(), np.random.rand()),
             arrowsize=15,
             width=2,
 
         )
+        plt.title('Linia numer {}'.format(1), fontsize=10)
+
     plt.show()
